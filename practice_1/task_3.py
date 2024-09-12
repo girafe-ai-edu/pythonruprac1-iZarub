94563854f67173b4f0e55c885401d719c79e3167
Ilia Zarubin <iz@terraquantum.swiss>
2024-09-12 21:16:55 +0300

    [ADD] Homework done

--- practice_1/task_3.py
+++ practice_1/task_3.py
@@ -1,13 +1,16 @@
-# -*- coding: utf-8 -*-
 """
 Напишите программу для расчета индекса массы тела (body mass index – 
 BMI) человека. Пользователь должен ввести свой рост и вес, после чего вы 
 используете одну из приведенных ниже формул для определения индекса.
 BMI = вес/рост**2 
 """
-weight = input()
-height = input()
 
 
-#Ваш кол
+def CalculateBMI(weight: float, height: float) -> float:
+    return weight/height**2
 
+if __name__ == "__main__":
+    weight: str = input()
+    height: str = input()
+
+    print(f'Your BMI is: {CalculateBMI(float(weight), float(height))}')
--- practice_1/task_4.py
+++ practice_1/task_4.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 """
 Разработайте программу, запрашивающую у  пользователя целое четырехзначное число и подсчитывающую сумму составляющих его цифр. Например, если пользователь введет число 3141,
  программа должна вывести 
@@ -6,3 +5,10 @@
 
 @author: Savant
 """
+
+def SumNumbers(number: str) -> int:
+    return sum([int(x) for x in number])
+
+if __name__ == "__main__":
+    number: str = input()  
+    print(SumNumbers(number))
